bf0dc41b9957bee1b81673257f50aec0f6e56511
Dexter C. Dalogdog <dx.dalogdog@gmail.com>
2023-02-01 14:59:58 +0800

    udpate on api

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! 0.1';
+    return `Hello World! 0.1, ${process.env.SOME_SAMPLE_SERVICE}`;
   }
 }
